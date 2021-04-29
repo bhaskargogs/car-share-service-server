b15a7ce60cee2a5389c89144fbe64aec39dd82fb
Bhaskar Gogoi <bhaskargogoi24@gmail.com>
2021-04-29 05:45:12 +0200

    Updated Controller test to purely mock RequestDTO

--- src/test/java/org/sharing/car/controller/DriverControllerTest.java
+++ src/test/java/org/sharing/car/controller/DriverControllerTest.java
@@ -3,11 +3,8 @@ package org.sharing.car.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.modelmapper.ModelMapper;
-import org.sharing.car.domainvalue.OnlineStatus;
 import org.sharing.car.dto.DriverCreationDTO;
 import org.sharing.car.dto.DriverDTO;
-import org.sharing.car.entity.Driver;
 import org.sharing.car.service.DriverService;
 import org.sharing.car.util.JsonMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,18 +13,16 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(DriverController.class)
+@Slf4j
 public class DriverControllerTest {
 
     @Autowired
@@ -47,16 +42,18 @@ public class DriverControllerTest {
 
     @Test
     public void testCreateValidDriver_Returns201() throws Exception {
-        DriverDTO driverDTO = new DriverDTO(null, "Allan", "Hufflepuff", "ahufflepuff@hotmail.com", "hotmail123", 43, "ONLINE", ZonedDateTime.now().withZoneSameInstant(ZoneId.of("UTC")), ZonedDateTime.now().withZoneSameInstant(ZoneId.of("UTC")));
-        when(driverService.createDriver(driverDTO)).thenAnswer(invocation -> invocation.getArgument(0));
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/drivers")
+        DriverCreationDTO creationDTO = new DriverCreationDTO("Allan", "Hufflepuff", "ahufflepuff@hotmail.com", "hotmail123", 43);
+        when(driverService.createDriver(any(DriverDTO.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        mockMvc.perform(MockMvcRequestBuilders.post("/drivers")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
-                .content(JsonMapper.mapToJson(driverDTO)))
+                .content(JsonMapper.mapToJson(creationDTO)))
                 .andExpect(status().isCreated())
-                .andReturn();
-
-        assertEquals(driverDTO, JsonMapper.mapFromJson(result.getResponse().getContentAsString(), DriverDTO.class));
-
+                .andExpect(jsonPath("$.firstName").value("Allan"))
+                .andExpect(jsonPath("$.lastName").value("Hufflepuff"))
+                .andExpect(jsonPath("$.email").value("ahufflepuff@hotmail.com"))
+                .andExpect(jsonPath("$.age").value(43))
+                .andExpect(jsonPath("$.firstName").value("Allan"))
+                .andExpect(jsonPath("$.status").value("ONLINE"));
     }
 }
